92759aaea8be61f995e6879263946f5f5204dafb
Shadulla Khurshid Alam Shaikh <110664358+sshadulla22@users.noreply.github.com>
2025-09-21 20:21:49 +0530

    Add files via upload

---
+++ src/components/CodeCompiler.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import "./CodeCompiler.css"; // optional for custom CSS
+
+function CodeCompiler() {
+  return (
+    <div className="under-dev-container">
+      <h1>Phase - 2 Development</h1>
+      <h2>Code Compiler</h2>
+      <p>⚠️ This tool is currently under development.</p>
+      <p>Stay tuned for updates!</p>
+      <p>HTML/CSS/C/C++/C#/Python/Java/JavaScript/...more</p>
+      <p>Stay Tune...</p>
+      
+    </div>
+  );
+}
+
+export default CodeCompiler;
---
+++ src/components/UtilityTools.js
@@ -0,0 +1,25 @@
+import React from "react";
+import "./UtilityTools.css"; // optional for custom CSS
+
+function UtilityTools() {
+  return (
+    <div className="under-dev-container">
+      <h1>Phase - 2 Development</h1>
+      <h2>Utility Tools</h2>
+      <p>⚠️ This tool is currently under development.</p>
+      <p>Stay tuned for updates!</p>
+      <ol>
+        <li>Unit Converter (Length, Weight, Temperature)</li>
+        <li>Currency Converter</li>
+        <li>Date / Time Formatter</li>
+        <li>Random Password Generator</li>
+        <li>Lorem Ipsum Generator</li>
+        <li>Many More....Under research</li>
+      </ol>
+      <p>Stay Tune...</p>
+      
+    </div>
+  );
+}
+
+export default UtilityTools;
