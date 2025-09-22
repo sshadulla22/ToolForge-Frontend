71ee2c90173f0b4d3c7ba132bf35d70b85ce83b7
Shadulla Khurshid Alam Shaikh <110664358+sshadulla22@users.noreply.github.com>
2025-09-22 20:55:44 +0530

    Add files via upload

---
+++ src/components/TopBar.js
@@ -0,0 +1,32 @@
+import React, { useState } from "react";
+import "./TopBar.css";
+import logo from "../Group 16.png";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars, faXmark } from "@fortawesome/free-solid-svg-icons";
+
+
+export default function TopBar() {
+  const [open, setOpen] = useState(false);
+
+  return (
+    <nav className="navbar">
+      <div className="nav-container">
+        <img src={logo} height="45"/>
+
+        <ul className={`nav-links ${open ? "open" : ""}`}>
+          {/* <li><a href="#">Pages</a></li>
+          <li><a href="#">Account</a></li>
+          <li><a href="#">Blocks</a></li>
+          <li><a href="#">Docs</a></li> */}
+        </ul>
+
+        <button className="get-started">Get Started</button>
+
+        {/* Toggle Button */}
+        <button className="menu-toggle" onClick={() => setOpen(!open)}>
+          {open ? <FontAwesomeIcon icon={faXmark} />  : <FontAwesomeIcon icon={faBars} />}
+        </button>
+      </div>
+    </nav>
+  );
+}
