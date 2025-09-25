1d3cd8ccf5ccc9298e8bb82bfeedb906a08852c1
Shadulla Khurshid Alam Shaikh <110664358+sshadulla22@users.noreply.github.com>
2025-09-25 17:37:35 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import LandingPage from "./components/LandingPage";
+import TopBar from "./components/TopBar";
+
+function Root() {
+  const [showApp, setShowApp] = useState(false);
+
+  return (
+    <>
+      {showApp ? <App /> : <LandingPage onContinue={() => setShowApp(true)} />}
+    </>
+  );
+}
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+root.render(<Root />);
